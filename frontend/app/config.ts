2e5385bfb65e2b7528b766aa5672ae8431e874ea
DALJEET SINGH <daljeetsingh@DALJEETs-MacBook-Air.local>
2025-08-11 22:14:33 +0530

    test

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,4 +1,4 @@
 
-// export const BACKEND_URL = "https://axon-primary-backend.onrender.com"
+export const BACKEND_URL = "https://axon-primary-backend.onrender.com"
 export const HOOKS_URL = "https://axon-4yb0.onrender.com"
-export const BACKEND_URL = "http://localhost:3000"
+// export const BACKEND_URL = "http://localhost:3000"
